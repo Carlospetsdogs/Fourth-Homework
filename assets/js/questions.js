78c6e9c682954f85347843e1e89021c107a7da46
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-02-28 10:33:12 -0500

    added first ?s to js sheet

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,17 @@
 // ?s, choices and answers
 var questions = [
     {
+        title: 'What does the acronym DOM stand for?'
+        choices: ['Document Odd Macro', 'Discern Object Mode', 'Document Object Model','Dont Observe Me']
+        answer: 'Document Object Model',
+    },
+    {
+        title: 'What element does not belong in an if/else statement?'
+        choices: ['else','else if','for','true']
+        answer: 'for',
+    },
+    {
         title: ''
-        choices:
-        answer:
-    },
-    {
-        title:
-        choices:
-        answer:
-    },
-    {
-        title:
         choices:
         answer:
     },
