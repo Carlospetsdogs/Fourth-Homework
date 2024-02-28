19f44bda66e4687e9b94ea53e41ce5f1cf2afa0b
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-02-28 10:45:32 -0500

    added more the the questions js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,43 @@
 // ?s, choices and answers
 var questions = [
     {
-        title: 'What does the acronym DOM stand for?'
-        choices: ['Document Odd Macro', 'Discern Object Mode', 'Document Object Model','Dont Observe Me']
+        title: 'What does the acronym DOM stand for?',
+        choices: ['Document Odd Macro', 'Discern Object Mode', 'Document Object Model','Dont Observe Me'],
         answer: 'Document Object Model',
     },
     {
-        title: 'What element does not belong in an if/else statement?'
-        choices: ['else','else if','for','true']
+        title: 'What element does not belong in an if/else statement?',
+        choices: ['else','else if','for','true'],
         answer: 'for',
     },
     {
-        title: ''
-        choices:
-        answer:
+        title: 'What value do Arrays begin with?',
+        choices: ['0','1','5','100'],
+        answer: '0',
     },
     {
-        title:
-        choices:
-        answer:
+        title: 'What does the Prevent Default method do?',
+        choices: [
+            'Prevents the default action of an event from occurring.',
+            'Keeps the code from breaking no matter what',
+            'Allows you to utilize the DOM more efficiently',
+            'Prevents event listeners from ever being utilized',
+        ],
+        answer: 'prevent the default action of an event from occurring.',
     },
     {
-        title:
-        choices:
-        answer:
+        title: 'What is the main purpose of the console.log function?',
+        choices: [
+            'Applying styles to CSS',
+            'Used to initiate a git push',
+            'Clearing out a block of code',
+            'Debugging and monitoring purposes during development', 
+        ],
+        answer: 'Debugging and monitoring purposes during development',
     },
     {
-        title: 'Are you impressed with my coding skills?'
-        choices: ['Yes','No','Honestly you are amazing',]
+        title: 'Are you impressed with my coding skills?',
+        choices: ['Yes','No','Honestly you are amazing'],
         answer: 'Honestly you are amazing',
     }
 ];
