5ca45acca5ee213749103fbe6620d5ff33b4be74
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-02-28 10:23:03 -0500

    added js to the questions sheet

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// ?s, choices and answers
+var questions = [
+    {
+        title: ''
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title: 'Are you impressed with my coding skills?'
+        choices: ['Yes','No','Honestly you are amazing',]
+        answer: 'Honestly you are amazing',
+    }
+];
